f0e240a9906cff8ef1f54345688c0404a5bf25e8
Tdust <gomestdust@gmail.com>
2017-03-29 13:07:38 -0300

    Aplicando bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'pagina/home'
+
   resources :vendas
   resources :jogos
   resources :clientes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pagina#home'
 end
